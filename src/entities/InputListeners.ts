9030289a85b105ac481a2799a694e698b1047496
Дмитрий Пасхин <d.pasxin@gmail.com>
2022-01-23 02:26:48 +0300

    feat(event-listeners): Reduce count of inner event listeners

--- src/entities/InputListeners.ts
+++ src/entities/InputListeners.ts
@@ -1,21 +1,11 @@
-/* eslint-disable unicorn/no-array-for-each,@typescript-eslint/unbound-method,@typescript-eslint/method-signature-style */
 import { SelectionRange } from '@src/entities/SelectionRange';
 import { InputChanger } from '@src/entities/InputChanger';
 import { InputValue } from '@src/entities/InputValue';
 
 export class InputListeners {
-  private readonly listenersMap: Array<{
-    name: keyof HTMLElementEventMap;
-    handler(event: HTMLElementEventMap[keyof HTMLElementEventMap]): void;
-  }> = [
-    { name: 'click', handler: this.onClick.bind(this) },
-    { name: 'select', handler: this.onSelect.bind(this) },
-    { name: 'keyup', handler: this.onKeyup.bind(this) },
-    { name: 'input', handler: this.onInput.bind(this) },
-  ];
-
   private userListenersMap: Array<{
     name: keyof HTMLElementEventMap;
+    // eslint-disable-next-line @typescript-eslint/method-signature-style
     handler(event: HTMLElementEventMap[keyof HTMLElementEventMap]): void;
   }> = [];
 
@@ -27,21 +17,20 @@ export class InputListeners {
   ) {}
 
   public init(): void {
-    this.listenersMap.forEach((listener) => {
-      this.$input.addEventListener(listener.name, listener.handler);
-    });
+    this.$input.addEventListener('input', this.onInput);
+    document.addEventListener('selectionchange', this.onSelectionChange);
   }
 
   public destroy(): void {
-    this.listenersMap.forEach((listener) => {
+    for (const listener of this.userListenersMap) {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
       this.$input.removeEventListener(listener.name, listener.handler);
-    });
-
-    this.userListenersMap.forEach((listener) => {
-      this.$input.removeEventListener(listener.name, listener.handler);
-    });
+    }
 
     this.userListenersMap = [];
+
+    this.$input.removeEventListener('input', this.onInput);
+    document.removeEventListener('selectionchange', this.onSelectionChange);
   }
 
   public on<Name extends keyof HTMLElementEventMap>(
@@ -56,45 +45,31 @@ export class InputListeners {
   public off(name: keyof HTMLElementEventMap): void {
     const updatedUserListeners = [];
 
-    // eslint-disable-next-line no-restricted-syntax
     for (const userListener of this.userListenersMap) {
       if (name !== userListener.name) {
         updatedUserListeners.push(userListener);
 
-        // eslint-disable-next-line no-continue
         continue;
       }
 
+      // eslint-disable-next-line @typescript-eslint/unbound-method
       this.$input.removeEventListener(userListener.name, userListener.handler);
     }
 
     this.userListenersMap = updatedUserListeners;
   }
 
-  private onClick(): void {
-    this.selectionRange.syncPrevious();
-  }
-
-  private onSelect(): void {
-    this.selectionRange.syncPrevious();
-  }
-
-  private onKeyup(event: KeyboardEvent): void {
-    if (event.code === 'ArrowLeft' || event.code === 'ArrowRight') {
-      this.selectionRange.syncPrevious();
-    }
-  }
-
-  private onInput(): void {
-    const possibleCursorPosition =
+  private onInput = (): void => {
+    const cursorPosition =
       this.selectionRange.previous.start === this.selectionRange.previous.end
         ? this.inputChanger.processSingleChange()
         : this.inputChanger.processMultiChange();
 
-    this.selectionRange.safeRangeSetter(possibleCursorPosition);
     this.inputValue.setFromChars();
-    this.selectionRange.safePreviousSetter(possibleCursorPosition);
-  }
-}
+    this.selectionRange.update(cursorPosition);
+  };
 
-/* eslint-enable */
+  private onSelectionChange = (): void => {
+    this.selectionRange.syncPrevious();
+  };
+}
