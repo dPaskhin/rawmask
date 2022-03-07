445f30204f6fb2902b9a35f90b4d61f73cbdb4d7
Дмитрий Пасхин <d.pasxin@gmail.com>
2022-03-08 02:12:17 +0300

    feat(skeleton): Delete babel transform runtime

--- babel.config.js
+++ babel.config.js
@@ -8,13 +8,5 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    '@babel/plugin-transform-typescript',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        regenerator: true,
-      },
-    ],
-  ],
+  plugins: ['@babel/plugin-transform-typescript'],
 };
