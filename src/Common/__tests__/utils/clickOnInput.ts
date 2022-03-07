6fabdfa4840c702272deda76b61da8c5dfee6e08
Дмитрий Пасхин <d.pasxin@gmail.com>
2022-03-08 02:18:04 +0300

    fix(test): Fix setting cursor position when click on the input

--- src/Common/__tests__/utils/clickOnInput.ts
+++ src/Common/__tests__/utils/clickOnInput.ts
@@ -4,6 +4,15 @@ export const clickOnInput = (
   $input: HTMLInputElement,
   cursorPosition = 0,
 ): void => {
-  $input.setSelectionRange(cursorPosition, cursorPosition);
+  /** Click on the input sets cursor position at last symbol in a value */
   userEvent.click($input);
+
+  /** The cursor position is set explicitly here  */
+  $input.setSelectionRange(cursorPosition, cursorPosition);
+
+  /**
+   * Click on input emits selection change event in a browser
+   * But it's not happen in test environment, so it needs to be emitting explicitly
+   */
+  document.dispatchEvent(new Event('selectionchange'));
 };
