42f03279d67f02e17dcec0d297d0614b6876ca03
Дмитрий Пасхин <d.pasxin@gmail.com>
2022-07-09 23:32:14 +0300

    refactor(utils): Simplify array from range creator

--- src/Common/utils/createArrayFromRange.ts
+++ src/Common/utils/createArrayFromRange.ts
@@ -1,6 +1,8 @@
 export const createArrayFromRange = (range: [number, number]): number[] => {
-  const start = Math.min(...range);
-  const end = Math.max(...range);
+  const start = range[0];
+  const end = range[1];
 
-  return Array.from({ length: end - start }).map((_, idx) => idx + start);
+  return Array.from({ length: Math.abs(end - start) }).map(
+    (_, idx) => idx + start,
+  );
 };
