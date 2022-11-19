1c1444f88427419360d7f4718da1c1d0cb85bab7
Dmitry Paskhin <d.pasxin@gmail.com>
2022-11-20 03:29:28 +0500

    docs(skeleton): Add docs comment to public entities

--- packages/rawmask/src/main/Common/types/TMask.ts
+++ packages/rawmask/src/main/Common/types/TMask.ts
@@ -1,3 +1,9 @@
+/**
+ * Prepared mask contains only array of escaped string and RegExp
+ */
 export type TDetailedMask = Array<string | RegExp>;
 
+/**
+ * Raw mask contains string or array of string/RegExp.
+ */
 export type TMask = string | TDetailedMask;
--- packages/rawmask/src/main/Rawmask/types/IRawmask.ts
+++ packages/rawmask/src/main/Rawmask/types/IRawmask.ts
@@ -1,9 +1,29 @@
 import { TMask } from '../../Common/types/TMask';
 
 export interface IRawmask {
+  /**
+   * Raw (unmasked) value of input.
+   * Getter and setter.
+   */
   rawValue: string;
+  /**
+   * Value of input.
+   * Getter and setter.
+   */
   value: string;
+  /**
+   * Mask of input.
+   * Getter and setter. You can dynamically change mask in any moment.
+   */
   mask: TMask;
+  /**
+   * Method for adding input event listeners.
+   *
+   * @param name - Name from all native event map.
+   * @param handler - Custom event handler, that takes instance of rawmask as context and as parameter, also native event.
+   *
+   * @return IRawmask
+   */
   on: <Name extends keyof HTMLElementEventMap>(
     name: Name,
     handler: (
@@ -12,6 +32,16 @@ export interface IRawmask {
       event: HTMLElementEventMap[Name],
     ) => void,
   ) => IRawmask;
+  /**
+   * Method for removing all event listeners from input by event name.
+   *
+   * @param name - Name from all native event map.
+   *
+   * @return void
+   */
   off: (name: keyof HTMLElementEventMap) => void;
+  /**
+   * Remove all listeners from input and set input value to empty string.
+   */
   destroy: () => void;
 }
